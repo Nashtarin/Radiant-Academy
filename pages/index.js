bd3c188593e3992e503d98a49c24130f2900bb35
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-02-07 17:27:56 +0600

    Head tag added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className=''>
+    <div>
+      <Head>
+        <title>Radiant Academy</title>
+        <meta name="Your learning partner" content="Best learning app for the curious learners" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1 className='text-4xl text-blue-600 font-bold text-center mt-5'>This is a heading</h1>
     </div>
   )
