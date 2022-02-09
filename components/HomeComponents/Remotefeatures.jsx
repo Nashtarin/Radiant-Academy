781e72b9d143e1b9f113c68024f03195e62223b0
Nashtarin Nur <nashtarin.nur@gmail.com>
2022-02-09 15:22:45 +0600

    75% done

--- components/HomeComponents/Remotefeatures.jsx
+++ components/HomeComponents/Remotefeatures.jsx
@@ -1,25 +1,54 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import p1 from '../images/1.png'
+import p2 from '../images/2.png'
+import p3 from '../images/3.png'
 
 const Remotefeatures = () => {
     return (
-        <div className="container" >
-            <h1 className="text-violet-700 text-3xl font-bold">Remote Features</h1>
-            <div>
-                <div>
+        <div className="container mx-auto" >
+            <h1 className="text-violet-700 text-3xl font-bold mx-96 mt-4">Remote Features</h1>
+            <div className='grid grid-cols-3  my-8 m-10 gap-x-4'>
+                <div className='bg-slate-100 rounded-lg'>
                     <Image
                         src={p1}
                         alt="Landscape picture"
-                        width={100}
-                        height={100}
+                        width="100px"
+                        height="100px"
+                        className='ml-96'
                     />
+                    <h3 className='text-center'>Quiz System</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo consectetur quis facere, pariatur quibusdam!</p>
+                    <Link href="/learn">
+                        <a>Learn More</a>
+                    </Link>
                 </div>
-                {/* <div>
-                    <img src="../images/2.png" alt="" />
+                <div className='bg-slate-100 rounded-lg'>
+                    <Image
+                        src={p2}
+                        alt="Landscape picture"
+                        width="100px"
+                        height="100px"
+                    />
+                    <h3 className="text-center">Code Editor</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta eligendi, sequi voluptates odio architecto ex.</p>
+                    <Link href="/learn">
+                        <a>Learn More</a>
+                    </Link>
                 </div>
-                <div>
-                    <img src="../images/3.png" alt="" />
-                </div> */}
+                <div className='bg-slate-100 rounded-lg'>
+                    <Image
+                        src={p3}
+                        alt="Landscape picture"
+                        width="100px"
+                        height="100px"
+                    />
+                    <h3 className="text-center">Online Forum</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur pariatur eaque velit praesentium nulla officiis!</p>
+                    <Link href="/learn">
+                        <a>Learn More</a>
+                    </Link>
+                </div>
             </div>
 
         </div>
