3a19f92189dd104285e31ccd43c9983652e7a0b4
intisar eusha <iftikharrasha@gmail.com>
2022-02-10 18:48:35 +0600

    slick slider issue fixed

--- components/HomeComponents/Slider.jsx
+++ components/HomeComponents/Slider.jsx
@@ -1,7 +1,39 @@
 import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderSection = () => {
-
+    var settings = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 700,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+            },
+        ],
+    };
 
     return (
         <>
@@ -9,10 +41,24 @@ const SliderSection = () => {
                 <div className="flex flex-col justify-center items-center lg:flex-row lg:my-20">
                     <div className="w-5/6 lg:w-[50%] py-14 xl:pl-10">
                         <div>
-                            <div>
-                                <h2 className="text-6xl font-bold">Problem Solving</h2>
-                                <p className="py-5 text-2xl lg:w-3/4 xl:w-1/2">We have got 20+ online solutions for programmers, who is excited?</p>
-                            </div>
+                            <Slider {...settings}>
+                                <div>
+                                    <h2 className="text-6xl font-bold">Problem Solving</h2>
+                                    <p className="py-5 text-2xl lg:w-3/4 xl:w-1/2">We have got 20+ online solutions for programmers, who is excited?</p>
+                                </div>
+
+                                
+                                <div>
+                                    <h2 className="text-6xl font-bold">Quiz System</h2>
+                                    <p className="py-5 text-2xl lg:w-3/4 xl:w-1/2">We have got 20+ online solutions for programmers, who is excited?</p>
+                                </div>
+
+                                
+                                <div>
+                                    <h2 className="text-6xl font-bold">Code Editor</h2>
+                                    <p className="py-5 text-2xl lg:w-3/4 xl:w-1/2">We have got 20+ online solutions for programmers, who is excited?</p>
+                                </div>
+                            </Slider>
                         </div>
                         <button className="bg-rose-500 rounded-md text-white px-7 py-3 my-5">
                             Try it out
