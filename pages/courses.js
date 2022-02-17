e07a9614fda2f4ae09f63a05384c4832a63d3597
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-02-18 00:33:49 +0600

    necessary changes in secondary branch

--- pages/courses.js
+++ pages/courses.js
@@ -1,25 +1,26 @@
 import React, { useEffect } from 'react';
 import Course from '../components/Course/Course';
 
-export const getServerSideProps = async ()=>{
+export const getServerSideProps = async () => {
     const url = 'https://api.itbook.store/1.0/new';
     const res = await fetch(url);
     const data = await res.json();
 
-    return{
-        props:{
-            data:data.books
+    return {
+        props: {
+            data: data.books
         }
     }
 }
-const Courses = ({data}) => {
+const Courses = ({ data }) => {
     return (
         <div className='container mx-auto'>
-            <h1 className='text-6xl	font-bold text-violet-900 mb-5 text-center'>Our Awesome Courses{data.length}</h1>
-            <div className="grid container mx-auto my-4 grid-rows-auto lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4">
-                {data.map(course=><Course
+            <h1 className='text-5xl	font-bold text-violet-900 mb-5 pt-14 text-center'>Our Awesome Courses</h1>
+            <h2 className='text-center text-2xl pb-8'> Course found: {data.length}</h2>
+            <div className="grid w-5/6 mx-auto my-4 grid-rows-auto lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+                {data.map(course => <Course
                     key={course.id}
-                    course={course}/>)}
+                    course={course} />)}
             </div>
             {/* <div className="grid container mx-auto grid-rows-auto lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4">
                 <div>01</div>
