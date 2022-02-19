817209a954b472b335cfa70f57a19931a44445f4
intisar eusha <iftikharrasha@gmail.com>
2022-02-19 23:10:03 +0600

    minor conflict removal

--- components/Course/Course.js
+++ components/Course/Course.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-=======
->>>>>>> Jahid
 import Link from "next/link";
 
 const Course = ({ course }) => {
