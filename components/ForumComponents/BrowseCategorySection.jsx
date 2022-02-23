91bcc47e5787faae1e873b6028a5a75c3f96e858
intisar eusha <iftikharrasha@gmail.com>
2022-02-23 16:47:53 +0600

    category cards made dynamic

--- components/ForumComponents/BrowseCategorySection.jsx
+++ components/ForumComponents/BrowseCategorySection.jsx
@@ -4,6 +4,7 @@ import {
   BsUmbrella
 } from "react-icons/bs";
 import Link from 'next/link';
+import categoryData from "../../utilities/FakeData/category.json";
 
 const BrowseCategorySection = () => {
   return (
@@ -19,48 +20,20 @@ const BrowseCategorySection = () => {
 
 
         <div className='grid container mx-auto my-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4'>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
-          <div className=' bg-slate-200 rounded-md drop-shadow-md'>
-            <div className='flex m-5 px-5'>
-              <span className='mt-1 mr-2 text-2xl'><BsUmbrella /></span>
-              <h1 className='font-bold text-lg'>Welcome</h1>
-            </div>
-            <p className='font-semifold text-base m-5 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore rerum voluptatum, maxime ipsum ut.</p>
-          </div>
+          { 
+                categoryData.map(category => 
+                  <Link href="/forum/result-page"  key={category.no} passHref>
+                    <div className=' bg-slate-200 rounded-md drop-shadow-md cursor-pointer'>
+                      <div className='flex m-5 px-5'>
+                        <span className='mt-1 mr-2 text-2xl'><img src={category.icon} alt={category.subTitile} /></span>
+                        <h1 className='font-bold text-lg'>{category.title}</h1>
+                      </div>
+                      <p className='font-semifold text-base m-5 px-5'>{category.desc}</p>
+                    </div>
+                  </Link>
+            
+                )
+            }
         </div>
       </div>
     </section>
