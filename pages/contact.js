25550b1277221dcd66b3f9dd48311a1d3071b70e
emon2058 <ashad2058@gmail.com>
2022-02-24 14:57:11 +0600

    email notification working

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import emailjs from '@emailjs/browser';
 
 const contact = () => {
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'radiant_academy', e.target, 'user_YkJkSWKqQwdoELEotZTxP')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
     return (
         <div>
             <div className="grid grid-cols-1 mx-auto py-[5rem]">
@@ -8,7 +20,7 @@ const contact = () => {
                     <h1 className="text-4xl font-bold text-center">Contact with us!</h1>
                 </div>
                 <div className="px-7 md:w-3/5 lg:w-3/6 xl:w-2/6 flex flex-col mx-auto">
-                    <form>
+                    <form onSubmit={sendEmail}>
                         <input
                             type="text"
                             name="name"
@@ -32,6 +44,7 @@ const contact = () => {
                         />
                         <textarea
                             className="bg-slate-200 border-2 w-full h-[120px] outline-none py-2 px-3 resize-none rounded-lg mb-4"
+                            name="message"
                             placeholder="Type your comment here!"
                             required
                         ></textarea>
