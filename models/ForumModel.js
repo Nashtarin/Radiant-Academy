6bad64821de2852c0a395afb7688b2f81740c00f
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-02-24 12:13:00 +0600

    Forums API created

---
+++ models/ForumModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+const ForumSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    authorImg: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        required: true,
+    },
+    loves: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    views: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+},
+
+    { timestamps: true }
+);
+
+export default mongoose.models.Forum || mongoose.model("Forum", ForumSchema);
