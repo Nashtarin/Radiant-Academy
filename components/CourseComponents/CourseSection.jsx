2c53c446281558ce678c2379fa5e3e45e5eea8dc
iftakherone <54303402+iftakherone@users.noreply.github.com>
2022-03-02 13:25:25 +0600

    added hide/show option

--- components/CourseComponents/CourseSection.jsx
+++ components/CourseComponents/CourseSection.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CourseCard from './CourseCard';
-import { FiPlusCircle } from 'react-icons/fi';
+import { FiPlusCircle, FiMinusCircle } from 'react-icons/fi';
 
 const CourseSection = () => {
+    const [show, setShow] = useState(true);
+
+    const handleShow = () => {
+        setShow(true)
+    }
+    const handleHide = () => {
+        setShow(false)
+    }
+
     return (
         <div className='container mx-auto'>
             <h1 className='text-4xl	font-bold text-violet-900 mt-10 mb-2 py-8 text-center'>
@@ -12,19 +21,27 @@ const CourseSection = () => {
             <div className="px-16 pb-20">
                 <div>
                     <div className="p-2 mb-1 flex items-center">
-                        <FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" />
+                        <label className="swap swap-flip">
+                            <input type="checkbox" className="hidden" />
+                            <div className="swap-off"><FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleShow} /></div>
+                            <div className="swap-on"><FiMinusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleHide} /></div>
+                        </label>
                         <h3 className="text-2xl font-medium"> Category Name</h3>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                    {show && <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                         <CourseCard />
                         <CourseCard />
                         <CourseCard />
                         <CourseCard />
-                    </div>
+                    </div>}
                 </div>
                 <div className="my-3">
                     <div className="p-2 mb-1 flex items-center">
-                        <FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" />
+                        <label className="swap swap-flip">
+                            <input type="checkbox" className="hidden" />
+                            <div className="swap-off"><FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleShow} /></div>
+                            <div className="swap-on"><FiMinusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleHide} /></div>
+                        </label>
                         <h3 className="text-2xl font-medium"> Category Name</h3>
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
@@ -36,7 +53,11 @@ const CourseSection = () => {
                 </div>
                 <div>
                     <div className="p-2 mb-1 flex items-center">
-                        <FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" />
+                        <label className="swap swap-flip">
+                            <input type="checkbox" className="hidden" />
+                            <div className="swap-off"><FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleShow} /></div>
+                            <div className="swap-on"><FiMinusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleHide} /></div>
+                        </label>
                         <h3 className="text-2xl font-medium"> Category Name</h3>
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
@@ -48,7 +69,11 @@ const CourseSection = () => {
                 </div>
                 <div className="my-3">
                     <div className="p-2 mb-1 flex items-center">
-                        <FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" />
+                        <label className="swap swap-flip">
+                            <input type="checkbox" className="hidden" />
+                            <div className="swap-off"><FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleShow} /></div>
+                            <div className="swap-on"><FiMinusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleHide} /></div>
+                        </label>
                         <h3 className="text-2xl font-medium"> Category Name</h3>
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
@@ -60,7 +85,11 @@ const CourseSection = () => {
                 </div>
                 <div>
                     <div className="p-2 mb-1 flex items-center">
-                        <FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" />
+                        <label className="swap swap-flip">
+                            <input type="checkbox" className="hidden" />
+                            <div className="swap-off"><FiPlusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleShow} /></div>
+                            <div className="swap-on"><FiMinusCircle className="text-xl font-bold mr-2 cursor-pointer" onClick={handleHide} /></div>
+                        </label>
                         <h3 className="text-2xl font-medium"> Category Name</h3>
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
