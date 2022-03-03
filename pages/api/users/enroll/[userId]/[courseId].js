0bb9be044df3e4379a027774729af0cd68dfc377
intisar eusha <iftikharrasha@gmail.com>
2022-03-03 19:58:41 +0600

    enrolledCourses api complexity resolved

--- pages/api/users/enroll/[userId]/[courseId].js
+++ pages/api/users/enroll/[userId]/[courseId].js
@@ -3,17 +3,18 @@ import User from '../../../../../models/UserModel'
 
 export default async function handler(req, res) {
     const { method, query: { userId, courseId } } = req;
-    console.log(userId, courseId);
+    // const data = req.body;
 
     await dbConnect()
 
     if (method === "PUT") {
         const filter = {_id: userId};
-        const update =  { $set : { "enrolledCourses.$.courseId": courseId} };
-        // const update =  { $inc : {'loves' : 1} };
+        const update =  { $push : { "enrolledCourses": 
+                                            {"courseId" : courseId}} 
+                                  };
 
         try {
-            const updatedUser = await User.updateOne(filter, update);
+            const updatedUser = await User.findOneAndUpdate(filter, update);
             res.status(201).json(updatedUser);
         } catch (error) {
             res.status(500).json({ success: false });
