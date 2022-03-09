9ffc759cba3e3fd40484a0b05b5c323cb8d2eac9
emon2058 <ashad2058@gmail.com>
2022-03-10 02:34:52 +0600

    comment course-content

--- pages/my-course/index.js
+++ pages/my-course/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Head from 'next/head';
-import MyCourseSection from '../../components/MyCourseComponents/MyCourseSection';
+// import React from 'react';
+// import Head from 'next/head';
+// import MyCourseSection from '../../components/MyCourseComponents/MyCourseSection';
 
-const Index = () => {
-    return (
-        <div>
+// const Index = () => {
+//     return (
+//         <div>
 
-            <MyCourseSection />
-        </div>
-    );
-};
+//             <MyCourseSection />
+//         </div>
+//     );
+// };
 
-export default Index;
+// export default Index;
